57f97f69ddf95a71bb723ccf0b453374301b6641
vidyashreevvce <145644659+vidyashreevvce@users.noreply.github.com>
2025-03-10 10:06:17 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,16 @@
-class Test
-{
-    public static void main(String []args)
-    {
-        System.out.println("My First Java Program.");
+import java.util.Scanner;
+
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        int sum = num1 + num2;
+        System.out.println("The sum is: " + sum);
     }
-};
+}
