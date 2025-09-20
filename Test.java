4a7c4e26a39e0ca72e6b5b328fdc758cc7fd03d3
vidyashreevvce <145644659+vidyashreevvce@users.noreply.github.com>
2025-09-20 11:26:08 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,15 +1,7 @@
-import java.util.Scanner;
-
 public class Test {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter first number: ");
-        int num1 = sc.nextInt();
-
-        System.out.print("Enter second number: ");
-        int num2 = sc.nextInt();
-
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
         int sum = num1 + num2;
         System.out.println("The sum is: " + sum);
     }
